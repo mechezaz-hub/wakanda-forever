299e1b9c5de0446e062121316e6de4b018dd49a1
KOUSER BIBI <mechezaz@gmail.com>
2026-05-05 19:14:03 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,72 @@
+def on_on_score():
+    NAMOR.set_position(70, 47)
+    NAMOR.say_text("OWIE! YOU KILLED ME! OK, OK, I GIVE UP! YOU ALL WIN!",
+        5000,
+        True)
+    NAMOR.destroy(effects.cool_radial, 1000)
+    scene.set_background_image(assets.image("""
+        boston-bridge
+        """))
+    effects.smiles.start_screen_effect()
+info.on_score(300, on_on_score)
+
+def on_on_overlap(sprite, otherSprite):
+    info.change_score_by(25)
+    NAMOR.set_position(148, 2)
+sprites.on_overlap(SpriteKind.guard, SpriteKind.enemy, on_on_overlap)
+
+def on_overlap_tile(sprite2, location):
+    game.game_over(False)
+scene.on_overlap_tile(SpriteKind.player,
+    sprites.dungeon.hazard_lava1,
+    on_overlap_tile)
+
+def on_overlap_tile2(sprite3, location2):
+    game.game_over(True)
+scene.on_overlap_tile(SpriteKind.player, sprites.jewels.jewel5, on_overlap_tile2)
+
+def on_on_overlap2(sprite4, otherSprite2):
+    scene.camera_shake(20, 5000)
+    shuri.say_text("OW! You Pierced Me!", 2000, True)
+    info.change_life_by(-5)
+    NAMOR.set_position(148, 2)
+    pause(2000)
+    for index in range(7000):
+        info.change_life_by(5)
+        pause(100)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap2)
+
+NAMOR: Sprite = None
+shuri: Sprite = None
+info.set_life(7000)
+scene.set_background_image(assets.image("""
+    wakanda
+    """))
+tiles.set_current_tilemap(tilemap("""
+    level2
+    """))
+scene.camera_follow_sprite(shuri)
+game.show_long_text("When game begins, press the ARROW KEYS to move Shuri, Okoye and Riri. If Namor catches you, you will lose points! ",
+    DialogLayout.FULL)
+shuri = sprites.create(assets.image("""
+    shuri
+    """), SpriteKind.player)
+controller.move_sprite(shuri)
+shuri.set_stay_in_screen(True)
+NAMOR = sprites.create(assets.image("""
+    namor
+    """), SpriteKind.enemy)
+NAMOR.set_position(148, 2)
+NAMOR.follow(shuri, 30.5)
+Riri = sprites.create(assets.image("""
+    riri
+    """), SpriteKind.guard)
+scene.camera_follow_sprite(Riri)
+Riri.set_stay_in_screen(True)
+controller.move_sprite(Riri, 34, -53)
+Okoye = sprites.create(assets.image("""
+    okoye
+    """), SpriteKind.guard)
+scene.camera_follow_sprite(Okoye)
+Okoye.set_stay_in_screen(True)
+controller.move_sprite(Okoye, -68, -58)
---
+++ main.ts
@@ -0,0 +1,63 @@
+info.onScore(300, function on_on_score() {
+    NAMOR.setPosition(70, 47)
+    NAMOR.sayText("OWIE! YOU KILLED ME! OK, OK, I GIVE UP! YOU ALL WIN!", 5000, true)
+    NAMOR.destroy(effects.coolRadial, 1000)
+    scene.setBackgroundImage(assets.image`
+        boston-bridge
+        `)
+    effects.smiles.startScreenEffect()
+})
+sprites.onOverlap(SpriteKind.Guard, SpriteKind.Enemy, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    info.changeScoreBy(25)
+    NAMOR.setPosition(148, 2)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function on_overlap_tile(sprite2: Sprite, location: tiles.Location) {
+    game.gameOver(false)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.jewels.jewel5, function on_overlap_tile2(sprite3: Sprite, location2: tiles.Location) {
+    game.gameOver(true)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_on_overlap2(sprite4: Sprite, otherSprite2: Sprite) {
+    scene.cameraShake(20, 5000)
+    shuri.sayText("OW! You Pierced Me!", 2000, true)
+    info.changeLifeBy(-5)
+    NAMOR.setPosition(148, 2)
+    pause(2000)
+    for (let index = 0; index < 7000; index++) {
+        info.changeLifeBy(5)
+        pause(100)
+    }
+})
+let NAMOR : Sprite = null
+let shuri : Sprite = null
+info.setLife(7000)
+scene.setBackgroundImage(assets.image`
+    wakanda
+    `)
+tiles.setCurrentTilemap(tilemap`
+    level2
+    `)
+scene.cameraFollowSprite(shuri)
+game.showLongText("When game begins, press the ARROW KEYS to move Shuri, Okoye and Riri. If Namor catches you, you will lose points! ", DialogLayout.Full)
+shuri = sprites.create(assets.image`
+    shuri
+    `, SpriteKind.Player)
+controller.moveSprite(shuri)
+shuri.setStayInScreen(true)
+NAMOR = sprites.create(assets.image`
+    namor
+    `, SpriteKind.Enemy)
+NAMOR.setPosition(148, 2)
+NAMOR.follow(shuri, 30.5)
+let Riri = sprites.create(assets.image`
+    riri
+    `, SpriteKind.Guard)
+scene.cameraFollowSprite(Riri)
+Riri.setStayInScreen(true)
+controller.moveSprite(Riri, 34, -53)
+let Okoye = sprites.create(assets.image`
+    okoye
+    `, SpriteKind.Guard)
+scene.cameraFollowSprite(Okoye)
+Okoye.setStayInScreen(true)
+controller.moveSprite(Okoye, -68, -58)
